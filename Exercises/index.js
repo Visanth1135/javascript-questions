9b489429a4d96f3216b38fb312711b19ef9b91e6
Visanth1135 <k.visanth1996@gmail.com>
2022-08-11 09:13:32 +0400

    call apply bind methods

--- Exercises/index.js
+++ Exercises/index.js
@@ -1,30 +1,52 @@
-// //memoize
-
-// const cs = (n1,n2)=>{
-//     for(let i=1;i<1000000000;i++){}
-//     return n1*n2
+// let name  = {
+//   first:'djhjf',
+//   last:'jhbs',
+//   print:function(){
+//     console.log(this.first + " " + this.last)
+//   }
 // }
 
-// console.time('first call');
-// console.log(cs(4,3))
-// console.timeEnd('first call')
+// let name2={
+//   first:'raju',
+//   last:'nair'
+// }
 
-const memoizedAdd = () => {
-    let cache = {};
-    return (n) => {
-      if (n in cache) {
-        console.log('Fetching from cache'+n);
-        return cache[n];
-      }
-      else {
-        console.log('Calculating result'+n);
-        let result = n + 10;
-        cache[n] = result;
-        console.log(cache)
-        return result;
-      }
-    }
+// name.print();// djhjf jhbs
+// name.print.call(name2) //raju nair
+
+//writing as seperate function the print method and using call method
+
+let name  = {
+  first:'djhjf',
+  last:'jhbs',
+}
+
+  let print=function(){
+    console.log(this.first + " " + this.last)
   }
-const newAdd = memoizedAdd();
-console.log(newAdd(9));
-console.log(newAdd(10))
+
+  let print2=function(home){
+    console.log(this.first + " " + this.last + home)
+  }
+
+  let print3=function(home,state,country){
+    console.log(this.first + " " + this.last + home + state + country)
+  }
+
+let name2={
+  first:'raju',
+  last:'nair'
+}
+
+print.call(name);// djhjf jhbs
+print.call(name2) //raju nair
+print2.call(name2,'kerala') //raju nairkerala
+print3.call(name2, 'kerala','kerala','india')//raju nairkeralakeralaindia
+
+//difference between call and apply is we pass directly strings in call method and apply we pass arguments as list
+print3.apply(name2,['kerala','kerala','india'])//raju nairkeralakeralaindia
+
+//difference between call and bind, bind gives you a copy of function and that can be call later
+let printname = print2.bind(name2,'kerala')
+console.log(printname)//console function print2
+printname()
